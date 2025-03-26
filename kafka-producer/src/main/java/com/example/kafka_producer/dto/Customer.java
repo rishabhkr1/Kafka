99c764f689ed5cd0bf89be25622b06d4df51c7bc
Rishabh Kumar <110474658+rishabhkr1@users.noreply.github.com>
2025-03-26 12:19:15 +0530

    kafka producer

---
+++ kafka-producer/src/main/java/com/example/kafka_producer/dto/Customer.java
@@ -0,0 +1,12 @@
+package com.example.kafka_producer.dto;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String contactNo;
+
+}
---
+++ kafka-producer/src/main/java/com/example/kafka_producer/service/KafkaMessagePublisher.java
@@ -0,0 +1,46 @@
+package com.example.kafka_producer.service;
+
+import com.example.kafka_producer.dto.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future = template.send("javatechie-demo", message);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+    public void sendEventsToTopic(Customer customer) {
+        try {
+            CompletableFuture<SendResult<String, Object>> future = template.send("javatechie-demo2", customer);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    System.out.println("Sent message=[" + customer.toString() +
+                            "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                } else {
+                    System.out.println("Unable to send message=[" +
+                            customer.toString() + "] due to : " + ex.getMessage());
+                }
+            });
+
+        } catch (Exception ex) {
+            System.out.println("ERROR : " + ex.getMessage());
+        }
+    }
+}
